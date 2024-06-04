88ac7fd38bca7b35cd8ebf08ac6d1e5f3f9e7f44
protech-tau <igemtau2024@gmail.com>
2024-06-04 19:13:34 +0000

    Update run_scoring.py

--- WebTool/run_scoring.py
+++ WebTool/run_scoring.py
@@ -1,4 +1,36 @@
+from utils.send_mail import send_mail
+from utils.pdf_generator import generate_pdf
+import sys
 
-# Peleg logic
 
+
+
+
+def main(trigger, gene, mrna, cell_type, email):
+    ###logic###
+
+
+
+
+
+
+    ## send results to client ##
+    pipeline_results = " "
+    generate_pdf(pipeline_results, f"./", order_by='')
+    send_mail(email, f"{pipeline_results.job_id}.pdf")
+
+
+
+
+
+if __name__ == '__main__':
+    # parameters from flask form
+    trigger = sys.argv[1]
+    gene = sys.argv[2]
+    email = sys.argv[3]
+    cell_type = sys.argv[4]
+    mrna = sys.argv[5]
+
+    # send to logic
+    main(trigger, gene, mrna, cell_type, email)
 
